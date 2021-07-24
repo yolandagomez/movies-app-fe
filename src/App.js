7bce7cfb603fad8e9a16517e7a9bc95d2454e2b3
yolandagomez <64153159+yolandagomez@users.noreply.github.com>
2021-07-24 14:06:15 +0200

    Layout html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+import "./App.css";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [fruit, setFruit] = useState("");
+
+  const handleFruitDropdownChange = (selectedFruit) => {
+    setFruit(selectedFruit);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movies App</h1>
+      <div>
+        <div>
+          <h2>Main header</h2>
+          <Dropdown
+            onChange={handleFruitDropdownChange}
+            options={["orange", "pear", "lemon"]}
+            value={fruit}
+          />
+          <Dropdown options={["tennis", "soccer", "basketball"]} />
+          <Dropdown options={["1", "2", "3"]} />
+
+          <div>
+            <img src="http://placekitten.com/800/200"></img>
+          </div>
+        </div>
+        <div>
+          <h2>
+            All movies displayed<i class="far fa-heart"></i>
+          </h2>
+          <span class="square"></span>
+          <span class="square">
+            <img src="http://placekitten.com/200/200"></img>
+            <div>
+              <button>
+                <i class="far fa-heart"></i>
+              </button>
+              <button>Add to Watch List</button>
+              <button>Not interested</button>
+            </div>
+          </span>
+          <span class="square">
+            <img src="http://placekitten.com/200/200"></img>
+          </span>
+          <span class="square">
+            <img src="http://placekitten.com/200/200"></img>
+          </span>
+          <span class="square">
+            <img src="http://placekitten.com/200/200"></img>
+          </span>
+          <span class="square">
+            <img src="http://placekitten.com/200/200"></img>
+          </span>
+          <span class="square">
+            <img src="http://placekitten.com/200/200"></img>
+          </span>
+          <span class="square">
+            <img src="http://placekitten.com/200/200"></img>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
